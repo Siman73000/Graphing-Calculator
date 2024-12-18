7a4f3989f30b05aa07538607edd8c0afdb48c200
Simon_Hamilton <51251489+Siman73000@users.noreply.github.com>
2024-12-17 23:12:31 -0600

    Delete Calc-in-Rust/project directory

--- Calc-in-Rust/project/target/debug/deps/unicode_ident-9dc34cd645ae5bf6.d
+++
@@ -1,8 +0,0 @@
-C:\Users\simon.hamilton\Documents\Repos\Graphing-Calculator\Calc-in-Rust\project\target\debug\deps\libunicode_ident-9dc34cd645ae5bf6.rmeta: C:\Users\simon.hamilton\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src/lib.rs C:\Users\simon.hamilton\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src\tables.rs
-
-C:\Users\simon.hamilton\Documents\Repos\Graphing-Calculator\Calc-in-Rust\project\target\debug\deps\libunicode_ident-9dc34cd645ae5bf6.rlib: C:\Users\simon.hamilton\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src/lib.rs C:\Users\simon.hamilton\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src\tables.rs
-
-C:\Users\simon.hamilton\Documents\Repos\Graphing-Calculator\Calc-in-Rust\project\target\debug\deps\unicode_ident-9dc34cd645ae5bf6.d: C:\Users\simon.hamilton\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src/lib.rs C:\Users\simon.hamilton\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src\tables.rs
-
-C:\Users\simon.hamilton\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src/lib.rs:
-C:\Users\simon.hamilton\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src\tables.rs:
--- Calc-in-Rust/project/target/debug/deps/windows_x86_64_msvc-7442d06d8120f873.d
+++
@@ -1,7 +0,0 @@
-C:\Users\simon.hamilton\Documents\Repos\Graphing-Calculator\Calc-in-Rust\project\target\debug\deps\libwindows_x86_64_msvc-7442d06d8120f873.rmeta: C:\Users\simon.hamilton\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.6\src/lib.rs
-
-C:\Users\simon.hamilton\Documents\Repos\Graphing-Calculator\Calc-in-Rust\project\target\debug\deps\libwindows_x86_64_msvc-7442d06d8120f873.rlib: C:\Users\simon.hamilton\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.6\src/lib.rs
-
-C:\Users\simon.hamilton\Documents\Repos\Graphing-Calculator\Calc-in-Rust\project\target\debug\deps\windows_x86_64_msvc-7442d06d8120f873.d: C:\Users\simon.hamilton\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.6\src/lib.rs
-
-C:\Users\simon.hamilton\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.6\src/lib.rs:
